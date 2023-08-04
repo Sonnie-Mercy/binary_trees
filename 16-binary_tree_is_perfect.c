57ffbf3d8f15186eace987fe39888e7d929295b8
root <ndirangumercy4@gmail.com>
2023-08-03 23:44:03 -0700

    task 16 - fixed typo

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height, right height;
+	size_t left_height, right_height;
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
@@ -30,7 +30,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	size_t left height, right height;
+	size_t left_height, right_height;
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
